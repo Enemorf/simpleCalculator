db94fc461003ea9ab50bd741621024c4fdbd2c42
Victor <svkolychev@yandex.ru>
2023-07-31 22:17:30 +0300

    Initial commit

---
+++ src/test/java/pro/sky/simpleCalculator/SimpleCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.simpleCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
